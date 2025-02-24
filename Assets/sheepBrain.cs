19daaacb07e22ce953e0ff6771b5cae7f0689c02
AlexisCaroff <43877091+AlexisCaroff@users.noreply.github.com>
2025-02-24 17:26:44 +0100

    terrain tree working

--- Assets/sheepBrain.cs
+++ Assets/sheepBrain.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;
 public class sheepBrain : MonoBehaviour
 {
+    public float moveRadius = 10f; // Rayon dans lequel chercher une nouvelle destination
+    public float minWaitTime = 5f; // Temps minimum entre les déplacements
+    public float maxWaitTime = 10f; // Temps maximum entre les déplacements
     public AudioClip[] meeeSounds;
     private AudioSource audioSource;
     public float multiplicator = 6.0f;
@@ -12,7 +16,9 @@ public class sheepBrain : MonoBehaviour
     NavMeshAgent agent;
     public bool infield = false;
     public float lookRadius = 20f;
-   
+    Animator animator;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,11 +27,16 @@ public class sheepBrain : MonoBehaviour
         targetBerger = playerManger.instance.Player.transform;
         BergerMove = playerManger.instance.Player.GetComponent<playerController>();
         audioSource = GetComponent<AudioSource>();
+        animator = GetComponentInChildren<Animator>();
+        StartCoroutine(sheepstupid());
     }
 
     // Update is called once per frame
     void Update()
     {
+            float speed = agent.velocity.magnitude;
+            animator.SetFloat("runsheep", speed);
+        
         float distancedog = Vector3.Distance(targetDog.position, transform.position);
         float distanceBerger = Vector3.Distance(targetBerger.position, transform.position);
         
@@ -34,6 +45,7 @@ public class sheepBrain : MonoBehaviour
            // Debug.Log("move away from dog");
             Vector3 direction = -(targetDog.position - transform.position).normalized;
             Vector3 Rundirection = new Vector3(direction.x * multiplicator, direction.y * multiplicator, direction.z * multiplicator);
+            agent.speed = 10;
             agent.SetDestination(transform.position + Rundirection);
         }
         
@@ -51,6 +63,30 @@ public class sheepBrain : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+    IEnumerator sheepstupid()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+
+            Vector3 randomPoint = GetRandomPoint();
+            agent.speed = 5;
+            agent.SetDestination(randomPoint);
+        }
+    }
+    Vector3 GetRandomPoint()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * moveRadius; // Point aléatoire dans un rayon
+        randomDirection += transform.position; // L'appliquer autour de l'agent
+
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomDirection, out hit, moveRadius, NavMesh.AllAreas))
+        {
+            return hit.position; // Retourne un point valide sur le NavMesh
+        }
+
+        return transform.position; // Si pas de point valide, ne bouge pas
     }
     void PlayRandomBee()
     {
